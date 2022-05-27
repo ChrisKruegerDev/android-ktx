41e628b53cbace8156a7795f168d81693b28b3d3
Christian Krüger <15275971+chrisnkrueger@users.noreply.github.com>
2022-05-27 14:16:36 +0200

    Add more Resources extensions

--- library/src/main/java/app/moviebase/ktx/content/ResourcesExtensions.kt
+++ library/src/main/java/app/moviebase/ktx/content/ResourcesExtensions.kt
@@ -6,8 +6,15 @@ import android.content.res.Resources
 val Int.dp: Int get() = (this / Resources.getSystem().displayMetrics.density).toInt()
 val Int.px: Int get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 
-// TODO: 1/13/21 remove this
+fun Float.toPx() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
+fun Int.toPx() = px
+fun Int.toDp() = dp
+
+@Deprecated("Use without the input parameter", replaceWith = ReplaceWith("toPx"))
 fun Float.toPx(resources: Resources) = (this * resources.displayMetrics.density + 0.5f).toInt()
+@Deprecated("Use without the input parameter", replaceWith = ReplaceWith("toPx"))
 fun Float.toPx(context: Context) = toPx(context.resources)
+@Deprecated("Use without the input parameter", replaceWith = ReplaceWith("toPx"))
 fun Int.toPx(context: Context) = toFloat().toPx(context.resources)
+@Deprecated("Use without the input parameter", replaceWith = ReplaceWith("toPx"))
 fun Int.toPx(resources: Resources) = toFloat().toPx(resources)
